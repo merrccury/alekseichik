5a855cc658342bae3022e2f9a8a8853ec78f476c
merrc <merrccury@gmail.com>
2022-12-16 23:20:23 +0300

    Added plug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import {Container, Typography} from '@mui/material';
 import React from 'react';
+import {SxProps} from "@mui/system";
+import {Theme} from "@mui/material/styles";
+
+
+const textStyle: SxProps<Theme> = {
+    color: '#0F8',
+    fontFamily: "'Gugi', cursive",
+}
+
+const noSelect = 'noselect';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container>
+                <Typography className={noSelect} sx={textStyle}  align="center" mt={2}>
+                    Alekseichik
+                </Typography>
+            </Container>
+
+        </div>
+    );
 }
 
 export default App;
